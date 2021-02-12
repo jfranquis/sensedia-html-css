e15e187904670550d273e9b2402cf6e0567da3de
Janaina Franquis <janainafranquis@hotmail.com>
2021-02-11 21:27:38 -0300

    Primeiro projeto. Aula HTML e CSS

---
+++ Aula 1/aula-1.js
@@ -0,0 +1,13 @@
+/* Intro HTML
+Estático: Bem simples, programado em HTLM  e CSS fixo x Dinâmico possui diversas processamentos em servidor, consulta banco de dados, servidor
+
+HTML Hypertxt Markup Language : Desenvolver websites, criado por Tim Berners-lee, para fácil compreensão por humanos e maquina, como o Google e mecanismos de Crawler da Web.
+Em 1997, o HTML foi desenvolvido pelo W3C e regulamentado como orgão oficial.
+
+HTML não é linguagem de programação e sim de marcação: são linguagens que não são de forma comum
+
+Evolução do HTML 2 =1995 / 5 = 1997 / 4 = 1999 / 5 = 2014
+
+
+
+*/  
